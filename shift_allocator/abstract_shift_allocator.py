e374bb290591b5d3faf63f6200f83e707deab77b
girish.bhatta <girish.bhatta@alexsolutions.com.au>
2021-03-22 13:00:34 +1100

    adding some more comments for the abstract class

--- shift_allocator/abstract_shift_allocator.py
+++ shift_allocator/abstract_shift_allocator.py
@@ -3,11 +3,28 @@ from abc import ABC, abstractmethod
 
 
 class AbstractShiftAllocator(ABC):
+    """
+    Abstract Shift Allocator method that specifies the minimum mandatory actions for shift allocation
+    """
 
     @abstractmethod
     def check_allocation(self):
+        """
+        Method to check for allocation of a shift
+        :return: None
+        :rtype: None
+        """
         pass
 
     @abstractmethod
     def perform_allocation(self, shift, partition):
+        """
+        Method to perform allocation for the given shift and partition
+        :param shift: The time indicies of time column for which there should be a shift
+        :type shift: tuple
+        :param partition: The demand for the corresponding shifts
+        :type partition: tuple
+        :return:None
+        :rtype:None
+        """
         pass
